ca8fc9a3cf23d94cf252060d70cac6b397de62b9
richard-jalonen <60547943+richard-jalonen@users.noreply.github.com>
2024-04-07 02:30:54 +0300

    dockerized application

--- frontend/src/client/api.client.ts
+++ frontend/src/client/api.client.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import type { EventCreateRequest } from '@/models/Event'
 import type { PersonCreateRequest, PersonUpdateRequest } from '@/models/Person'
 
-const apiUrl = 'http://localhost:8080'
+const apiUrl = import.meta.env.VITE_API_URL
 
 export default class ApiClient {
   public static getPaymentOptions() {
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,13 +6,13 @@ import nightwatchPlugin from 'vite-plugin-nightwatch'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    nightwatchPlugin(),
-  ],
+  plugins: [vue(), nightwatchPlugin()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    host: '0.0.0.0'
   }
 })
